43f361d431c508f63e0507bf128de8096f95090d
Xenonius <haim@nissani.net>
2015-12-24 16:46:17 +0200

    1st round

---
+++ Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -0,0 +1,13 @@
+some_var = "false"
+another_var = "nil"
+
+if some_var == "false"
+  puts 'the content of some_var is indeed "false!"'
+end
+if another_var.nil?
+  puts "Question mark in the method name? - Yes - it is actually a function"
+end
+if some_var == "false"
+  puts "Looks like this one should execute"
+end 
+  puts "I guess nothing matched... But why?"
