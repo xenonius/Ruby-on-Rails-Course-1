b953ff97e8aac8bdd01206ed8c2cb84b9cdeab3e
Xenonius <haim@nissani.net>
2015-12-24 17:01:08 +0200

    2nd

--- Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,13 +1,15 @@
 some_var = "false"
 another_var = "nil"
 
-if some_var == "false"
-  puts 'the content of some_var is indeed "false!"'
-end
-if another_var.nil?
-  puts "Question mark in the method name? - Yes - it is actually a function"
-end
-if some_var == "false"
+if some_var == "pink elephant"
+  puts "Don't think about the pink elephant!"
+
+elsif another_var.nil?
+  puts "Question mark in the method name?"
+
+elsif some_var == false
   puts "Looks like this one should execute"
-end 
+
+else
   puts "I guess nothing matched... But why?"
+end
